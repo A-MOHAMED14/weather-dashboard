06aeed267e3ef7c5a4dcdd95842e9b2d368107b2
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-06-05 17:27:26 +0100

    Add DOM elements, global variables, and a click event listner on the search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,54 @@
+// DOM elements
+const cityInputEl = document.querySelector("#city-name");
+const searchBtnEl = document.querySelector("#search-btn");
+
+// Global variables
 const WEATHER_API_BASE_URL = "https://api.openweathermap.org";
 const WEATHER_API_KEY = "f23ee9deb4e1a7450f3157c44ed020e1";
 const MAX_DAILY_FORECAST = 5;
 
+searchBtnEl.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const city = cityInputEl.value;
+  doSomething(city);
+});
+
 // Lookup the location to get the Lat/Lon
-function doSomething() {
-  var apiUrl = `${WEATHER_API_BASE_URL}/geo/1.0/direct?q=${search}&limit=5&appid=${WEATHER_API_KEY}`;
+function doSomething(city) {
+  const apiUrl = `${WEATHER_API_BASE_URL}/geo/1.0/direct?q=${city}&limit=5&appid=${WEATHER_API_KEY}`;
   fetch(apiUrl)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      //   console.log(data, "<-----");
 
       // Pick the First location from the results
-      //const location = data[0];
-      var lat = data[0].lat;
-      var lon = data[0].lon;
+      const location = data[0];
+      const lat = location.lat;
+      const lon = location.lon;
+
+      //   console.log("location:", location, "lat:", lat, "lon:", lon);
 
       // Get the Weather for the cached location
-      var apiUrl = `${WEATHER_API_BASE_URL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${WEATHER_API_KEY}`;
+      const apiUrl = `${WEATHER_API_BASE_URL}/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&exclude=minutely,hourly&appid=${WEATHER_API_KEY}`;
 
       fetch(apiUrl)
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
-
+          //   console.log(data.current, "<=====");
+          const currentWeather = data.current;
+          const currentTemp = Math.floor(((currentWeather.temp - 32) * 5) / 9);
+          const currentHumidity = currentWeather.humidity;
+          const currentWindSpeed = currentWeather.wind_speed;
           // Show the Current Weather Forecast
+          console.log(
+            "Temp:",
+            Math.floor(((currentWeather.temp - 32) * 5) / 9),
+            "Humidity:",
+            currentWeather.humidity,
+            "Wind:",
+            currentWeather.wind_speed
+          );
 
           // Show the 5 Day Weather Forecast
         });
